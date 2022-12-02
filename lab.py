a6fb6f007619b4b5d5675e3d384fe6888ecad3ea
Akshat <akshat.22503@sscbs.du.ac.in>
2022-12-02 11:19:01 +0530

    test

---
+++ lab.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number: "))
+sum=a+b+c
+avg=sum/int(3)
+print("The sum of the three numbers is",sum)
+print("The average of the three numbers is",avg)
---
+++ lab1.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
+d=int(input("Enter the fourth number:"))
+ur_ex=a**6 + b/c + 7**d
+print("a**6 + b/c + 7**d = ",ur_ex )
